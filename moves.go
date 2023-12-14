3ff4206d9c630fb0acd677bc3da3be84acfb623d
Guillaume J. Charmes <git+guillaume@charmes.net>
2023-12-14 11:09:41 -0500

    Cleanup movements.

--- moves.go
+++ moves.go
@@ -3,42 +3,38 @@ package main
 import "go.creack.net/wolf3d/math2"
 
 func (g *Game) moveForward(s float64) {
-	if g.getTexNum(int(g.pos.X+g.dir.X*s), int(g.pos.Y)) == 0 {
-		g.pos.X += g.dir.X * s
+	if newX := g.pos.X + g.dir.X*s; g.getTexNum(int(newX), int(g.pos.Y)) == 0 {
+		g.pos.X = newX
 	}
-
-	if g.getTexNum(int(g.pos.X), int(g.pos.Y+g.dir.Y*s)) == 0 {
-		g.pos.Y += g.dir.Y * s
+	if newY := g.pos.Y + g.dir.Y*s; g.getTexNum(int(g.pos.X), int(newY)) == 0 {
+		g.pos.Y = newY
 	}
 }
 
 func (g *Game) moveLeft(s float64) {
-	if g.getTexNum(int(g.pos.X-g.plane.X*s), int(g.pos.Y)) == 0 {
-		g.pos.X -= g.plane.X * s
+	if newX := g.pos.X - g.plane.X*s; g.getTexNum(int(newX), int(g.pos.Y)) == 0 {
+		g.pos.X = newX
 	}
-
-	if g.getTexNum(int(g.pos.X), int(g.pos.Y-g.plane.Y*s)) == 0 {
-		g.pos.Y -= g.plane.Y * s
+	if newY := g.pos.Y - g.plane.Y*s; g.getTexNum(int(g.pos.X), int(newY)) == 0 {
+		g.pos.Y = newY
 	}
 }
 
 func (g *Game) moveBackwards(s float64) {
-	if g.getTexNum(int(g.pos.X-g.dir.X*s), int(g.pos.Y)) == 0 {
-		g.pos.X -= g.dir.X * s
+	if newX := g.pos.X - g.dir.X*s; g.getTexNum(int(newX), int(g.pos.Y)) == 0 {
+		g.pos.X = newX
 	}
-
-	if g.getTexNum(int(g.pos.X), int(g.pos.Y-g.dir.Y*s)) == 0 {
-		g.pos.Y -= g.dir.Y * s
+	if newY := g.pos.Y - g.dir.Y*s; g.getTexNum(int(g.pos.X), int(newY)) == 0 {
+		g.pos.Y = newY
 	}
 }
 
 func (g *Game) moveRight(s float64) {
-	if g.getTexNum(int(g.pos.X+g.plane.X*s), int(g.pos.Y)) == 0 {
-		g.pos.X += g.plane.X * s
+	if newX := g.pos.X + g.plane.X*s; g.getTexNum(int(newX), int(g.pos.Y)) == 0 {
+		g.pos.X = newX
 	}
-
-	if g.getTexNum(int(g.pos.X), int(g.pos.Y+g.plane.Y*s)) == 0 {
-		g.pos.Y += g.plane.Y * s
+	if newY := g.pos.Y + g.plane.Y*s; g.getTexNum(int(g.pos.X), int(newY)) == 0 {
+		g.pos.Y = newY
 	}
 }
 
